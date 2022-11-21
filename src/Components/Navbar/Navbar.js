d9bb7c519991dedb75dbb41bf609b3f9b545b62f
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-11-21 14:51:56 +0530

    Update and rename Navbar.tsx to Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -14,8 +14,8 @@ import {
     DrawerContent,
     Text,
     useDisclosure,
-    BoxProps,
-    FlexProps,
+    // BoxProps,
+    // FlexProps,
     Menu,
     MenuButton,
     MenuDivider,
@@ -32,14 +32,10 @@ import {
     // FiBell,
     FiChevronDown,
 } from 'react-icons/fi';
-import { IconType } from 'react-icons';
-import { ReactText } from 'react';
+// import { IconType } from 'react-icons';
+// import { ReactText } from 'react';
 
-interface LinkItemProps {
-    name: string;
-    icon: IconType;
-}
-const LinkItems: Array<LinkItemProps> = [
+const LinkItems = [
     { name: 'Home', icon: FiHome },
     { name: 'Products', icon: FiShoppingCart },
     { name: 'Wishlist', icon: FiStar },
@@ -48,8 +44,6 @@ const LinkItems: Array<LinkItemProps> = [
 
 export default function SidebarWithHeader({
     children,
-}: {
-    children: ReactNode;
 }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
@@ -79,11 +73,11 @@ export default function SidebarWithHeader({
     );
 }
 
-interface SidebarProps extends BoxProps {
-    onClose: () => void;
-}
+// interface SidebarProps extends BoxProps {
+//     onClose: () => void;
+// }
 
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+const SidebarContent = ({ onClose, ...rest }) => {
     return (
         <Box
             transition="3s ease"
@@ -109,11 +103,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     );
 };
 
-interface NavItemProps extends FlexProps {
-    icon: IconType;
-    children: ReactText;
-}
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+const NavItem = ({ icon, children, ...rest }) => {
     return (
         <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
             <Flex
@@ -145,11 +135,7 @@ const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
     );
 };
 
-interface MobileProps extends FlexProps {
-    onOpen: () => void;
-}
-
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+const MobileNav = ({ onOpen, ...rest }) => {
     return (
         <Flex
             ml={{ base: 0, md: 60 }}
@@ -218,7 +204,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
                             <MenuItem>Profile</MenuItem>
                             <MenuItem>Settings</MenuItem>
                             <MenuItem>Billing</MenuItem>
-                            <MenuDivider />
+                            <MenuDivider />1
                             <MenuItem>Sign out</MenuItem>
                         </MenuList>
                     </Menu>
