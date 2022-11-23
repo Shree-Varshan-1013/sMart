c87914b710286d57bc31cfd371cf3581e0d6ceab
SHREE VARSHAN G <113965644+Shree-Varshan-1013@users.noreply.github.com>
2022-11-23 10:02:35 +0530

    Delete src directory

--- src/theme.js
+++
@@ -1,9 +0,0 @@
-import { theme as chakraTheme } from '@chakra-ui/react'
-import { extendTheme } from "@chakra-ui/react"
-import "@fontsource/plus-jakarta-sans";
-
-const fonts = { ...chakraTheme.fonts, body: "Plus Jakarta Sans" };
-
-const theme = extendTheme({ fonts });
-
-export default theme;
